465931b79fb7caa0f475d3e81fd0982362938f21
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2022-04-15 12:34:51 +0000

    update test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid='text'
         >
          Hey yay!, Just a simple text to trigger build !
         </a>
