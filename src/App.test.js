be9b2a71500b68514bc467a0e6ed060311828b78
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2022-04-15 09:50:22 +0000

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link 2', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
